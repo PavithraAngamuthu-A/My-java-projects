b44de20bc2fc289fc5adc9f95901f0e1a16f9246
Pavithra. A <pavithrakumares@gmail.com>
2025-07-02 11:30:45 +0530

    1st commit

--- Check number a is present in b.java
+++ Check number a is present in b.java
@@ -2,43 +2,38 @@
  input a=234;b=123456
  output=yes //234 is present in b*/
 import java.util.*;
-public class CheckApresentB {
-    public static int check(int a,int[]a1){
+import static java.lang.System.exit;
+public class A70AisPresentB {
+    public static int check(int  n,int[]a1){
+
         int i=0;
-        while(a!=0){
-            a1[i++]=a%10;
-            a=a/10;
-        }return i;
+        while(n!=0){
+            a1[i++]=n%10;
+            n/=10;
+        }
+        return i;
     }
     public static void main(String[] args) {
-        Scanner s1 = new Scanner(System.in);
-        int a = s1.nextInt();
-        int b = s1.nextInt();
-        int[] a1 = new int[10];
-        int[] b1 = new int[10];
-        int len1 = check(a, a1);
-        int len2 = check(b, b1);
-        int count = 0, ans = 0;
-        for (int i = len1 - 1; i >= 0; i--) {
-            for (int j = len2 - 1; j >= 0; j--) {
-                if (a1[i] == b1[j] && a1[i + 1] == b1[j + 1] || i == len1 - 1) {
-                    count++;
-                    break;
-                } else {
-                    count = 0;
+        Scanner s1= new Scanner(System.in);
+        int i,j,count = 1,ans=0;
+        //int a=426,b=74286;
+        int a=s1.nextInt();
+        int b=s1.nextInt();
+        int []a1=new int[10];
+        int []b1=new int[10];
+        int l1=check(a,a1);
+        int l2=check(b,b1);
+        for(i=0;i<=l2-l1;i++){
+            count=1;
+            for(j=0;j<l1;j++){
+                if(b1[i+j]!=a1[j]){
+                    count=0;break;
                 }
             }
-            if (count == 0) {
-                ans = 0;
-            } else {
-                ans += count;
+            if(count==1){
+                System.out.println("Yes.A is present in B");return;
             }
         }
-        if (ans == len1) {
-            System.out.println("Yes,The number a is present in number b");
-        } else {
-            System.out.println("No,The number a is not present in number b");
-        }
+        System.out.println("No,A is not present in B");
     }
-
 }
